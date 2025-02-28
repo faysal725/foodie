f64ce1be6337254325327f05c58e16ea26d49506
faysal725 <afaysal725@gmail.com>
2025-02-28 11:54:31 +0600

    final push

--- app/meals/layout.js
+++ app/meals/layout.js
@@ -1,5 +1,8 @@
 import React from 'react'
-
+export const metadata = {
+  title: 'All Meals',
+  description: 'Browse the dilicious meals',
+};
 export default function MealsLayout({children}) {
   return (
     <>
